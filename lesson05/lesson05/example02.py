adf3aa9dc7fa614f1dccb7cb97322420ec75d50d
Jaidergan <Cerega4444@yandex.ru>
2021-12-05 11:57:21 +0300

    Lesson5
    Lesson5 in GeekBrains

--- lesson05/lesson05/example02.py
+++ lesson05/lesson05/example02.py
@@ -7,6 +7,11 @@ with open('example02.txt') as f:
     rows = f.readlines()
     expanded_rows = [row.split() for row in rows]
 
-rows_count, words_count = len(rows), sum([len(word_list) for word_list in expanded_rows])
+rows_count = len(rows)
+words_count = [len(word_list) for word_list in expanded_rows]
 
-print(f"Всего строк - {rows_count}, всего слов - {words_count}")
+print(f"Всего строк - {rows_count}")
+n = 0
+while n < len(words_count):
+    print(f"Количество слов в строке {n + 1} - {words_count[n]}")
+    n += 1
